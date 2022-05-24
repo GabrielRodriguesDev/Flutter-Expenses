32fca615c34ddc61979ad47b4cc384d4eb00a14e
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2022-05-24 01:06:21 -0300

    Organizando projeto

---
+++ lib/models/transacion.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: non_constant_identifier_names
+
+class Transsaction {
+  final String id;
+  final String title;
+  final double value;
+  final DateTime data;
+
+  Transsaction({
+    required this.id,
+    required this.title,
+    required this.value,
+    required this.data,
+  });
+}
